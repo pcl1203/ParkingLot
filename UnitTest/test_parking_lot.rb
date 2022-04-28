f5b79b70118a66a2aa30f297bb32330285b8cabb
pcl1203 <96214370+pcl1203@users.noreply.github.com>
2022-04-28 14:51:34 +0800

    added function definition and some unit testing

--- UnitTest/test_parking_lot.rb
+++ UnitTest/test_parking_lot.rb
@@ -18,4 +18,27 @@ class TestParkingLot < Test::Unit::TestCase
     assert_equal(0, @parking_lot.create('a'))
     assert_equal(0, @parking_lot.create('.././,'))
   end
+
+  test 'park' do
+    assert_equal(3, @parking_lot.create(3))
+    assert_equal(2, @parking_lot.park('abc-123', 'black'))
+    assert_equal(3, @parking_lot.park('hbc-122', 'white'))
+  end
+
+  test 'leaving car' do
+    assert_equal(3, @parking_lot.create(3))
+    assert_equal(1, @parking_lot.leave(1))
+  end
+
+  test 'color_based_status' do
+    ret = @parking_lot.color_based_status(black)
+    assert_equal('ABC-123', ret[0])
+    assert_equal(0, ret[1])
+  end
+
+  test 'platenumber_based_status' do
+    ret = @parking_lot.platenumber_based_status('ABC-123')
+    assert_equal('white', ret[0])
+    assert_equal(0, ret[1])
+  end
 end
--- parking_lot.rb
+++ parking_lot.rb
@@ -3,13 +3,63 @@
 # frozen_string_literal: true
 
 # Parking Lot Class
-class ParkingLot 
-  def create(lots = 1) # Creating Parking Lots
+class ParkingLot
+  # Creating Parking Lots
+  def create(lots = 1)
+    @vacantslot = 1
     lots_to_create = Integer(lots)
     puts "Created #{lots_to_create} lot/s"
     lots_to_create
   rescue StandardError
     puts "Invalid lot number used: #{lots}"
     0
+  end
+
+  # Park Cars with its platenumber and color
+  def park(platenumber, color)
+    puts "#{color} vehicle with platenumber #{platenumber} parked on slot #{@vacantslot}"
+    @vacantslot += 1
+    @vacantslot
+  rescue StandardError
+    puts 'Car not parked'
+    @vacantslot
+  end
+
+  # Car Leaving based on slot number
+  def leave(slot)
+    puts "Vehicle on slot #{slot} has been left"
+    slot
+  rescue StandardError
+    puts 'No car is leaving'
+    slot
+  end
+
+  # Get the details of parked cars
+  def status
+    puts "#{color} vehicle with platenumber #{platenumber} on slot #{slot} has been left"
+  rescue StandardError
+    puts 'No cars parked'
+  end
+
+  # Get the details of parked cars based on its color
+  def color_based_status(color)
+    platenumber = 'ABC-123'
+    parked_on_slot = 0
+    puts "#{color} vehicle with platenumber #{platenumber} on slot #{slot} has been left"
+    [platenumber, parked_on_slot]
+  rescue StandardError
+    puts "No #{color} cars parked"
+    [platenumber, parked_on_slot]
+  end
+
+  # Get the details of parked cars based on its plate number
+  def platenumber_based_status(platenumber)
+    color = 'white'
+    parked_on_slot = 0
+    puts "#{color} vehicle with platenumber #{platenumber} on slot #{slot} has been left"
+    [color, parked_on_slot]
+  rescue StandardError
+    puts "No car with plate number #{platenumber} parked"
+    [color, parked_on_slot]
   end
 end
