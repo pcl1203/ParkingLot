3ffc934e48cb6527d5674d1518086ae4183ad866
pcl1203 <96214370+pcl1203@users.noreply.github.com>
2022-04-30 17:27:06 +0800

    added commands

--- main.rb
+++ main.rb
@@ -3,9 +3,11 @@
 # frozen_string_literal: true
 
 require './parking_lot'
+require './constants'
 
 puts 'Parking Lot Ticketing'
 finish = 0
+created = false
 parking_lot = ParkingLot.new
 while finish.zero?
   option = gets
@@ -14,9 +16,59 @@ while finish.zero?
 
   command = words[0]
 
+  if !created 
+    case command
+    when 'lot', 'create_parking_lot'
+      if created
+        puts 'Already initiated'
+      elsif words.count >= 2
+        parking_lot.create(words[1])
+        created = true
+      else
+        puts "Incomplete Command: #{option}"
+      end
+      next
+    else
+      puts 'Try creating parking lots first'
+    end
+  end
+
+  # ensure that lots are already created
+  next if !created
+
   case command
-  when 'lot'
-    parking_lot.create(words[1]) if words.count == 2
+  when 'park'
+    if words.count >= 3
+      parking_lot.park(words[1], words[2])
+    else
+      puts "Incomplete Command: #{option}"
+    end
+  when 'leave'
+    if words.count >= 2
+      parking_lot.leave_on_slot(words[1])
+    else
+      puts "Incomplete Command: #{option}"
+    end
+  when 'stat', 'status'
+    parking_lot.status
+  when 'cc', 'cars_with_colour'
+    if words.count >= 2
+      parking_lot.color_based_status(words[1])
+    else
+      puts "Incomplete Command: #{option}"
+    end
+  when 'cpn', 'car_with_platenumber', 'slot_number_for_registration_number'
+    if words.count >= 2
+      parking_lot.platenumber_based_status(words[1])
+    else
+      puts "Incomplete Command: #{option}"
+    end
+  when 'pwc', 'plate_numbers_for_cars_with_colour'
+    if words.count >= 2
+      parking_lot.plate_num_with_color(words[1])
+    else
+      puts "Incomplete Command: #{option}"
+    end
   when 'end'
     finish = 1
   else
